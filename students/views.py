c8ee7baac9abd949079ccfc5f5dfe04c9a168762
chenyw16 <ustcwen@163.com>
2020-10-23 14:18:05 +0800

    drf学习2

--- students/views.py
+++ students/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from students.models import Student
+from students.serializers import StudentModelSerializer
 # Create your views here.
+class StuentAPIView(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentModelSerializer
